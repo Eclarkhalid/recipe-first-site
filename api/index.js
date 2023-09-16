beedf210b52cbf629f5f70c600fcd75bdfdc7e4f
Eclarkhalid <eclarkhalid@gmail.com>
2023-09-16 15:13:55 +0300

    updating index.js

--- api/index.js
+++ api/index.js
@@ -34,6 +34,23 @@ const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
 app.use(cors({ credentials: true, origin: 'https://recipe-first-site-8g48dsqka-eclarkhalid.vercel.app' }));
+
+const corsOptions = {
+  credentials: true,
+  origin: 'https://recipe-first-site-8g48dsqka-eclarkhalid.vercel.app',
+};
+
+app.options('*', cors(corsOptions));
+
+
+app.use((err, req, res, next) => {
+  if (err.name === 'CorsError') {
+    res.status(403).json({ message: 'CORS error' });
+  } else {
+    next(err);
+  }
+});
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
